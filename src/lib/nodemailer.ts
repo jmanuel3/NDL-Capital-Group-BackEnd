3993ff6dc0b7d3511b94812012f4d6c05c9448eb
jmanuel3 <josem199806@gmail.com>
2026-06-09 20:02:30 +0200

    fix/nodemailer update

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: "info@ndlcapitalgroup.com",
     pass: process.env.EMAIL_PASSWORD,
